29d4156b85b6425afcf0aecf967811e0f855c1c2
Your Name <rajkumarsahu2002@gmail.com>
2023-04-23 20:44:22 +0530

    steeleye assignment

---
+++ src/component/List.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect, memo } from "react";
+import PropTypes from "prop-types";
+
+// Single List Item
+const WrappedSingleListItem = ({ index, isSelected, onClickHandler, text }) => {
+  return (
+    <li
+      style={{
+        backgroundColor: isSelected ? "green" : "red",
+      }}
+      onClick={() => onClickHandler(index)} // modified
+    >
+      {text}
+    </li>
+  );
+};
+
+WrappedSingleListItem.propTypes = {
+  index: PropTypes.number,
+  isSelected: PropTypes.bool,
+  onClickHandler: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+const SingleListItem = memo(WrappedSingleListItem);
+
+// List Component
+const WrappedListComponent = ({ items }) => {
+  const [selectedIndex, setSelectedIndex] = useState(null); // modified
+
+  useEffect(() => {
+    setSelectedIndex(null);
+  }, [items]);
+
+  const handleClick = (index) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <ul style={{ textAlign: "left" }}>
+      {items.map((item, index) => (
+        <SingleListItem
+          key={index}
+          onClickHandler={() => handleClick(index)}
+          text={item.text}
+          index={index}
+          isSelected={selectedIndex === index} // modified
+        />
+      ))}
+    </ul>
+  );
+};
+
+WrappedListComponent.propTypes = {
+  items: PropTypes.arrayOf(
+    // modified
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+WrappedListComponent.defaultProps = {
+  items: [
+    { text: "FrontEnd | Steeleye" },
+    { text: "BackEnd | Steeleye" },
+    { text: "Data Engineer | Steeleye" },
+  ],
+};
+
+const List = memo(WrappedListComponent);
+
+export default List;
